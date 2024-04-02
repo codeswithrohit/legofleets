ac65d571a58be0717bb45d39ede49c2965c6f798
codeswithrohit <hrohit320@gmail.com>
2024-04-02 13:40:43 +0530

    sumitted booking

--- pages/Admin/Booking.js
+++ pages/Admin/Booking.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { firebase } from '../../Firebase/config';
-
+import { useRouter } from "next/router";
 const Booking = () => {
   const [bookings, setBookings] = useState([]);
   const [loading, setLoading] = useState(true); // State variable to track loading status
-
+  const [isAdmin, setIsAdmin] = useState(false);
+  const router = useRouter(); 
+  // Check if the user is an admin when the component mounts
+  useEffect(() => {
+    const isAdminInLocalStorage = localStorage.getItem("isAdmin") === "true";
+    setIsAdmin(isAdminInLocalStorage);
+    if (!isAdminInLocalStorage) {
+      // If the user is not an admin, show a loading message or redirect them to the login page
+      router.push("/Admin/adminlogin");
+    } else {
+    }
+  }, [router]);
   useEffect(() => {
     const fetchBookings = async () => {
       try {
